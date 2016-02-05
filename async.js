8dca469948b664c2ab252642df3c893d8e5dd27c
Gaurav Singh <sgaurav.baghel@gmail.com>
2016-02-05 17:19:38 +0530

    super simple example for async

--- async.js
+++ async.js
@@ -1,3 +1,13 @@
 /**
+ * Stage: Proposal
+ * Full proposal: https://github.com/tc39/ecmascript-asyncawait
  * 
+ * Async functions allow you to `await` on a promise.
+ * `await` halts function in a non blocking way, waits for resolve and returns a value.
  */
+
+async function fetchDetails(){
+  var details = await functionThatReturnsAPromise();
+  console.log(details); // response from previously called function
+}
+
